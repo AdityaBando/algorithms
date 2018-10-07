2d914858c926c9ec66a0f44817d20fe1aa668d73
Aditya Bando <adityabando8@gamil.com>
2018-10-07 11:14:43 +0530

    added push()

--- ds-stack-linked-list.cpp
+++ ds-stack-linked-list.cpp
@@ -18,3 +18,17 @@ int element_traversal(element* ptr){
     ptr= ptr->next;
   }
 }
+
+int push(stack s,int x){
+  if (s.list==NULL) {
+    s.list=new element;
+    s.top=&(s.list);
+    (s.list)->next=NULL;
+  }
+  else{
+    element* p_ele=new element;
+    s.top=&p_ele;
+    p_ele->next=s.list;
+    s.list=*s.top;
+  }
+}
