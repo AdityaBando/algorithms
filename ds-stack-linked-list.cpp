f7f462b98a394e36507264a82148621b96e052cf
Aditya Bando <adityabando8@gamil.com>
2018-10-07 11:43:08 +0530

    modified push

--- ds-stack-linked-list.cpp
+++ ds-stack-linked-list.cpp
@@ -13,22 +13,37 @@ typedef struct stack{
 
 
 int element_traversal(element* ptr){
+  std::cout << "{" << ' ';
+
   while (ptr!=NULL) {
-    std::cout << ptr->data << '\n';
+    std::cout << ptr->data << ' ';
     ptr= ptr->next;
   }
+
+  std::cout << "}" << '\n';
 }
 
-int push(stack s,int x){
+element* push(stack s,int x){
   if (s.list==NULL) {
     s.list=new element;
     s.top=&(s.list);
+    (s.list)->data=x;
     (s.list)->next=NULL;
   }
   else{
     element* p_ele=new element;
     s.top=&p_ele;
+    p_ele->data=x;
     p_ele->next=s.list;
     s.list=*s.top;
   }
+  return s.list;
+}
+
+int main(int argc, char const *argv[]) {
+  stack s;
+
+  element_traversal(push(s,1));
+  element_traversal(push(s,2));
+  return 0;
 }
